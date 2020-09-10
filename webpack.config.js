90322a97acf27e5c15f625e67080001942db3f4b
Alexander Vasiliev <selado@ya.ru>
2020-09-10 21:03:59 +0300

    fix image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,9 @@ module.exports = {
                 quality: [0.65, 0.90],
                 speed: 4
               },
+              gifsicle: {
+                interlaced: false,
+              },
             }
           },
         ]
